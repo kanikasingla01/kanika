5586c662415d883f42c4202f1174488f7e296344
Kanika Singla <Kanika.Singla@winshuttle.com>
2020-10-03 13:19:26 +0530

    initial changes to project

---
+++ LinkedinTests.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using OpenQA.Selenium.Interactions; 
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    [TestFixture]
+    public class LinkedinTests
+    {
+       static IWebDriver driver;
+        PageActions pageActions = new PageActions();
+        Validations validations = new Validations();
+
+        public String email = "testaccount@gmail.com"; // Replace with valid registered email on linkedin
+         public String password = "pass"; // Replace with valid password used when registering
+         public String username = "JohnWack"; // Replace with valid username used when registering
+         public String fullName = "John Wack";
+         public String locality = "Macedonia";
+         public String linkedinURL = "https://www.linkedin.com/";
+
+         [SetUp]
+         public void SetUp()
+         {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait=TimeSpan.FromSeconds(30);    //ImplicitWait(TimeSpan.FromSeconds(30));
+        }
+            [Test]
+         public void SmokeTest()
+         {
+            pageActions.navigateToLinkedin(driver, linkedinURL);
+            pageActions.Login(driver, email, password);
+            pageActions.navigateThroughMenu(driver, PageObjects.ProfileLinkMenu, PageObjects.editProfileLink);
+            validations.validateElementIsPresent(driver, PageObjects.addPhotoElement);
+            validations.validateTextInElement(driver, PageObjects.fullNameElement, fullName);
+            validations.validateTextInElement(driver, PageObjects.localityElement, locality);
+            pageActions.Logout(driver);
+        }
+
+         [Test]
+         [Ignore("Ignore a test")]
+        public void RegressionTest()
+         {
+
+         }
+
+         [TearDown]
+         public void TearDown()
+         {
+            driver.Quit();
+        } 
+}
+}
---
+++ PageActions.cs
@@ -0,0 +1,50 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+   public  class PageActions
+    {
+        BasicMethods basicMethod = new BasicMethods();
+         Validations validations = new Validations();
+
+         public void navigateToLinkedin(IWebDriver driver, String linkedinURL)
+         {
+         driver.Navigate().GoToUrl(linkedinURL);
+         validations.validateScreenByUrl(driver, linkedinURL);
+         validations.validateElementIsPresent(driver, PageObjects.forgotPassLink);
+         }
+
+        public void Login(IWebDriver driver, String email, String password)
+        {
+         basicMethod.clickElement(driver, PageObjects.loginEmailInput);
+         basicMethod.sendKeys(driver, PageObjects.loginEmailInput, email);
+         basicMethod.clickElement(driver, PageObjects.loginPassInput);
+         basicMethod.sendKeys(driver, PageObjects.loginPassInput, password);
+                    basicMethod.clickElement(driver, PageObjects.loginButton);
+         // Wait up to 30s for the user to login and to be redirected to Home screen
+         driver.Manage().Timeouts().ImplicitWait=TimeSpan.FromSeconds(30);
+         validations.validateElementIsPresent(driver, PageObjects.newsFeedElement);
+         }
+
+         public void navigateThroughMenu(IWebDriver driver, By menu, By submenu)
+         {
+         Actions builder = new Actions(driver);
+         builder.MoveToElement(driver.FindElement(menu)).Perform();
+         builder.MoveToElement(driver.FindElement(submenu)).Click().Perform();
+         }
+
+        public void Logout(IWebDriver driver)
+        {
+            Actions builder = new Actions(driver);
+            builder.MoveToElement(driver.FindElement(PageObjects.navProfilePhoto)
+            ).Perform();
+            builder.MoveToElement(driver.FindElement(PageObjects.logoutButton)).Click().Perform();
+             } 
+        }
+}
